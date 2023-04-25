170917df7ed1b7f9e4ee7580487928aefa064529
Yatin889 <yatin8895@gmail.com>
2023-04-25 22:05:15 +0530

    basic mobile Functionality done

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -8,6 +8,10 @@ public class CarController : MonoBehaviour
     float movement;
     float rotationMov;
     public float AngularSpeed=50f;
+    public float Brakemangnitude;
+
+    bool onBackButtonPressed = false;
+
     public WheelJoint2D backWheel;
     public WheelJoint2D frontWheel;
     Rigidbody2D carRB;
@@ -16,16 +20,18 @@ public class CarController : MonoBehaviour
     void Start()
     {
         carRB=GetComponent<Rigidbody2D>();
-        AngularSpeed = 40f;
+        AngularSpeed = 60f;
+        Brakemangnitude = 600f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        movement = Input.GetAxisRaw("Vertical") * speed;
-        rotationMov = Input.GetAxisRaw("Horizontal") * AngularSpeed;
+       // movement = Input.GetAxisRaw("Vertical") * speed;
+        //rotationMov = Input.GetAxisRaw("Horizontal") * AngularSpeed;
         Translation();
         Rotational();
+        BrakeApply();
     }
 
     private void Rotational()
@@ -41,10 +47,12 @@ public class CarController : MonoBehaviour
         if (movement == 0F)
         {
             frontWheel.useMotor = false;
-             backWheel.useMotor = false;
+            backWheel.useMotor = false;
+           
         }
         else
         {
+            Debug.Log("button working");
             frontWheel.useMotor = true;
             backWheel.useMotor = true;
 
@@ -54,7 +62,57 @@ public class CarController : MonoBehaviour
             //Value=0 is there in maxTorgue can't just assign one value and move on you have to assign both or all values of struct type  
             Bmotor.maxMotorTorque = 10000F;
             frontWheel.motor = Bmotor;
+          
             backWheel.motor = Bmotor;
         }
+    }
+    public void ButtonUP()
+    {
+        movement = 0F;
+        Translation();
+        onBackButtonPressed = false;
+        carRB.angularDrag = 20f;
+    } 
+        
+    public void ForwardButtonPressed()
+    {
+        movement = speed;
+        Debug.Log("forward btn clicked");
+        Translation();
+    } 
+    public void BackwardButtonPressed()
+    {
+        /*movement = -0.5f*speed;
+        Debug.Log("backward btn clicked");
+        Translation();
+        */
+        onBackButtonPressed = true;
+    }
+
+    private void BrakeApply()
+    {
+        if (onBackButtonPressed)
+        {
+            carRB.AddForce(Vector2.left * Brakemangnitude*Time.fixedDeltaTime, ForceMode2D.Force);
+            carRB.angularDrag = 1000f;
+            Debug.Log("pressed back");
+        }
+       
+    }
+
+    public void upRotationPressed()
+    {
+        rotationMov = -AngularSpeed;
+        Rotational();
+    }
+    public void downRotationPressed()
+    {
+        rotationMov = AngularSpeed;
+        Rotational();
+    }
+    public void RotationUpButton()
+    {
+        rotationMov = 0f;
+        Rotational();
     }
 }
